e7ddc3803780b6235fd192954003bf14bd8d7402
Suriya94454 <suriya94454@gmail.com>
2024-01-02 17:24:00 +0530

    Modified Data table

--- src/components/Leads/LeadsTab.js
+++ src/components/Leads/LeadsTab.js
@@ -1,54 +1,103 @@
 import React, { useState } from 'react';
 import { FaEdit } from 'react-icons/fa';
 import { Table, Form } from 'react-bootstrap';
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const data = [
     { id: 1, name: 'Suriya', company: 'CloudECS', email: 'suriya@example.com', phone: 9632587410, lead: 'stark' },
     { id: 2, name: 'Chandru', company: 'ECSCloud', email: 'Chandru@example.com', phone: 9658741235, lead: 'stark' },
     { id: 3, name: 'John Doe', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
     { id: 4, name: 'Jane Smith', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
-    { id: 5, name: 'Jane Smith', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
-    { id: 6, name: 'Jane Smith', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
+    { id: 5, name: 'Gayathri', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
+    { id: 6, name: 'Saravanan', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
+    { id: 7, name: 'Jane Smith', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
+    { id: 8, name: 'Jane Smith', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
+    { id: 9, name: 'Jane Smith', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
+    { id: 10, name: 'Jane Smith', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
+    { id: 11, name: 'Jane Smith', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
+    { id: 12, name: 'Jane Smith', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
+    { id: 13, name: 'Jane Smith', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
+    { id: 14, name: 'Jane Smith', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
+    { id: 15, name: 'Jane Smith', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
+    { id: 16, name: 'Poongodi', company: 'ECSCloud', email: 'jane.smith@example.com', phone: 9658741235, lead: 'stark' },
 ];
 
-const DataTable = ({ isSelectedAll, handleSelectAll, handleCheckboxChange, filteredData, sortColumn, sortDirection, handleSort }) => {
+const ITEMS_PER_PAGE = 7;
+
+const DataTable = ({ isSelectedAll, selectedItems, handleSelectAll, handleCheckboxChange, filteredData, sortColumn, sortDirection, handleSort, currentPage, setCurrentPage }) => {
+    const sortedData = [...filteredData].sort((a, b) => {
+        const aValue = a[sortColumn];
+        const bValue = b[sortColumn];
+
+        if (sortDirection === 'asc') {
+            if (aValue < bValue) return -1;
+            if (aValue > bValue) return 1;
+        } else {
+            if (aValue > bValue) return -1;
+            if (aValue < bValue) return 1;
+        }
+
+        return 0;
+    });
+
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const endIndex = startIndex + ITEMS_PER_PAGE;
+    const currentData = sortedData.slice(startIndex, endIndex);
+
     return (
-        <Table>
-            <thead className="table-header">
-                <tr>
-                    <th>
-                        <Form.Check
-                            type="checkbox"
-                            checked={isSelectedAll}
-                            onChange={handleSelectAll}
-                        />
-                    </th>
-                    <th onClick={() => handleSort('name')}>Lead Name {sortColumn === 'name' && (sortDirection === 'asc' ? '▲' : '▼')}</th>
-                    <th onClick={() => handleSort('company')}>Company {sortColumn === 'company' && (sortDirection === 'asc' ? '▲' : '▼')}</th>
-                    <th onClick={() => handleSort('email')}>Email {sortColumn === 'email' && (sortDirection === 'asc' ? '▲' : '▼')}</th>
-                    <th onClick={() => handleSort('phone')}>Phone Number {sortColumn === 'phone' && (sortDirection === 'asc' ? '▲' : '▼')}</th>
-                    <th onClick={() => handleSort('lead')}>Lead Owner {sortColumn === 'lead' && (sortDirection === 'asc' ? '▲' : '▼')}</th>
-                </tr>
-            </thead>
-            <tbody>
-                {filteredData.map((item) => (
-                    <tr key={item.id}>
-                        <td>
+        <>
+            <Table>
+                <thead className="table-header">
+                    <tr>
+                        <th>
                             <Form.Check
                                 type="checkbox"
                                 checked={isSelectedAll}
-                                onChange={() => handleCheckboxChange(item.id)}
+                                onChange={() => handleCheckboxChange('all')}
                             />
-                        </td>
-                        <td>{item.name}</td>
-                        <td>{item.company}</td>
-                        <td>{item.email}</td>
-                        <td>{item.phone}</td>
-                        <td>{item.lead}</td>
+                        </th>
+                        <th onClick={() => handleSort('name')}>Lead Name {sortColumn === 'name' && (sortDirection === 'asc' ? '▲' : '▼')}</th>
+                        <th onClick={() => handleSort('company')}>Company {sortColumn === 'company' && (sortDirection === 'asc' ? '▲' : '▼')}</th>
+                        <th onClick={() => handleSort('email')}>Email {sortColumn === 'email' && (sortDirection === 'asc' ? '▲' : '▼')}</th>
+                        <th onClick={() => handleSort('phone')}>Phone Number {sortColumn === 'phone' && (sortDirection === 'asc' ? '▲' : '▼')}</th>
+                        <th onClick={() => handleSort('lead')}>Lead Owner {sortColumn === 'lead' && (sortDirection === 'asc' ? '▲' : '▼')}</th>
                     </tr>
-                ))}
-            </tbody>
-        </Table>
+                </thead>
+                <tbody>
+                    {currentData.map((item) => (
+                        <tr key={item.id}>
+                            <td>
+                                <Form.Check
+                                    type="checkbox"
+                                    checked={isSelectedAll || selectedItems.includes(item.id)}
+                                    onChange={() => handleCheckboxChange(item.id)}
+                                />
+                            </td>
+                            <td>{item.name}</td>
+                            <td>{item.company}</td>
+                            <td>{item.email}</td>
+                            <td>{item.phone}</td>
+                            <td>{item.lead}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+            <div className="pagination d-flex justify-content-end">
+                <button
+                    onClick={() => setCurrentPage(currentPage - 1)}
+                    disabled={currentPage === 1}
+                >
+                    <IoIosArrowBack />
+                </button>
+                <span>{`Page ${currentPage}`}</span>
+                <button
+                    onClick={() => setCurrentPage(currentPage + 1)}
+                    disabled={endIndex >= filteredData.length}
+                >
+                    <IoIosArrowForward />
+                </button>
+            </div>
+        </>
     );
 };
 
@@ -59,6 +108,7 @@ function LeadsTab() {
     const [filteredData, setFilteredData] = useState(data);
     const [sortColumn, setSortColumn] = useState('name');
     const [sortDirection, setSortDirection] = useState('asc');
+    const [currentPage, setCurrentPage] = useState(1);
 
     const handleSelectAll = () => {
         setIsSelectedAll(!isSelectedAll);
@@ -66,45 +116,42 @@ function LeadsTab() {
     };
 
     const handleCheckboxChange = (itemId) => {
-        const updatedSelectedItems = isSelectedAll
-            ? selectedItems.filter((item) => item !== itemId)
-            : [...selectedItems, itemId];
+        if (itemId === 'all') {
+            const updatedSelectedItems = isSelectedAll ? [] : data.map((item) => item.id);
+            setIsSelectedAll(!isSelectedAll);
+            setSelectedItems(updatedSelectedItems);
+        } else {
+            const updatedSelectedItems = isSelectedAll
+                ? selectedItems.filter((item) => item !== itemId)
+                : selectedItems.includes(itemId)
+                    ? selectedItems.filter((item) => item !== itemId)
+                    : [...selectedItems, itemId];
 
-        setIsSelectedAll(updatedSelectedItems.length === data.length);
-        setSelectedItems(updatedSelectedItems);
+            setIsSelectedAll(updatedSelectedItems.length === data.length);
+            setSelectedItems(updatedSelectedItems);
+        }
     };
 
+
+
+
     const handleSearch = (value) => {
-        // Filter data based on the search term
         const result = data.filter((item) => item.name.toLowerCase().includes(value.toLowerCase()));
         setFilteredData(result);
+        setCurrentPage(1); // Reset to the first page when searching
     };
 
     const handleSort = (column) => {
-        // Sort the data based on the selected column
-        const direction = column === sortColumn && sortDirection === 'asc' ? 'desc' : 'asc';
         setSortColumn(column);
-        setSortDirection(direction);
-
-        const sortedData = [...filteredData].sort((a, b) => {
-            const valueA = a[column].toLowerCase();
-            const valueB = b[column].toLowerCase();
-
-            if (direction === 'asc') {
-                return valueA.localeCompare(valueB);
-            } else {
-                return valueB.localeCompare(valueA);
-            }
-        });
-
-        setFilteredData(sortedData);
+        setSortDirection((prevDirection) => (prevDirection === 'asc' ? 'desc' : 'asc'));
+        setCurrentPage(1); // Reset to the first page when sorting
     };
 
     return (
-        <section className='leadsTab' style={{ marginTop: '105px' }}>
-            <div className='container-fluid py-3'>
-                <div className='row'>
-                    <div className='col-lg-6 col-md-6 col-12'>
+        <section className="leadsTab" style={{ marginTop: '20px' }}>
+            <div className="container-fluid py-3">
+                <div className="row">
+                    <div className="col-lg-6 col-md-6 col-12">
                         <input
                             type="text"
                             placeholder="Search.."
@@ -114,9 +161,9 @@ function LeadsTab() {
                                 handleSearch(e.target.value);
                             }}
                         />
-                        <p className='mt-3'><b>Total Records: {filteredData.length}</b></p>
+                        <p className="mt-3"><b>Total Records: {filteredData.length}</b></p>
                     </div>
-                    <div className='col-lg-6 col-md-6 col-12'>
+                    <div className="col-lg-6 col-md-6 col-12">
                         <div className="dropdown d-flex flex-column align-items-end">
                             <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                                 Create Lead
@@ -127,11 +174,11 @@ function LeadsTab() {
                                 <a className="dropdown-item" href="/lead">Something else here</a>
                             </div>
                         </div>
-                        <div className='row mt-3'>
-                            <div className='col-lg-10 col-md-10 col-12 d-flex flex-column align-items-end'>
-                                <FaEdit className='editIcon' size={38} />
+                        <div className="row mt-3">
+                            <div className="col-lg-10 col-md-10 col-12 d-flex flex-column align-items-end">
+                                <FaEdit className="editIcon" size={38} />
                             </div>
-                            <div className='col-lg-2 col-md-2 col-12 d-flex flex-column align-items-end'>
+                            <div className="col-lg-2 col-md-2 col-12 d-flex flex-column align-items-end">
                                 <div className="dropdown">
                                     <button className="actionBtn btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
                                         Actions
@@ -146,15 +193,18 @@ function LeadsTab() {
                         </div>
                     </div>
                 </div>
-                <div className='mt-3'>
+                <div className="mt-3">
                     <DataTable
                         isSelectedAll={isSelectedAll}
+                        selectedItems={selectedItems}
                         handleSelectAll={handleSelectAll}
                         handleCheckboxChange={handleCheckboxChange}
                         filteredData={filteredData}
                         sortColumn={sortColumn}
                         sortDirection={sortDirection}
                         handleSort={handleSort}
+                        currentPage={currentPage}
+                        setCurrentPage={setCurrentPage}
                     />
                 </div>
             </div>
