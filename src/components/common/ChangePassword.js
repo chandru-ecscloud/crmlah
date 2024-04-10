17424a1d0e605f1052354b4c8fab6ab07f0e8613
harishragv110 <harishragav110@gmail.com>
2024-04-10 10:42:23 +0530

    login and change password

--- src/components/common/ChangePassword.js
+++ src/components/common/ChangePassword.js
@@ -5,40 +5,38 @@ import { API_URL } from "../../Config/URL";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-
 import * as yup from 'yup';
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from 'react-hook-form'
+import { useFormik } from "formik";
 
-const schema = yup.object().shape({
-  userName: yup.string().required("!please enter the UserName"),
-  // password: yup.string().required("!Enter the valid Password").min(4, "!min length of 4 chars").matches(
+const validationSchema = yup.object().shape({
+  userName: yup.string().required("*please enter the UserName"),
+  // password: yup.string().required("*Enter the valid Password").min(4, "!min length of 4 chars").matches(
   //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
   //   'Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character'
-  // ).max(10, "!Enter upto 15 chars only"),
+  // ).max(10, "*Enter upto 15 chars only"),
 
-  oldPassword: yup.string().required("!Enter the Old Password").min(4, "!min length of 4 chars").matches(
+  oldPassword: yup.string().required("*Enter the Old Password").min(4, "*min length of 4 chars").matches(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
     'Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character'
-  ).max(10, "!Enter upto 15 chars only"),
+  ).max(10, "*Enter upto 15 chars only"),
 
-  newPassword: yup.string().required("!Enter the New Password").min(4, "!min length of 4 chars").matches(
+  newPassword: yup.string().required("*Enter the New Password").min(4, "*min length of 4 chars").matches(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
     'Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character'
-  ).max(10, "!Enter upto 15 chars only"),
+  ).max(10, "*Enter upto 15 chars only"),
 
-  confirmPassword: yup.string().required("!Enter the Confirm Password").min(4, "!min length of 4 chars").matches(
+  confirmPassword: yup.string().required("*Enter the Confirm Password").min(4, "*min length of 4 chars").matches(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
     'Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character'
-  ).max(10, "!Enter upto 15 chars only"),
+  ).max(10, "*Enter upto 15 chars only"),
 });
 
 function ChangePassword() {
   const [showOldPassword, setShowOldPassword] = useState(false);
   const [showNewPassword, setShowNewPassword] = useState(false);
-  const [showCPassword, setShowCPasword] =useState(false);
-  const token = sessionStorage.getItem("token");   
-  const role = sessionStorage.getItem('role');
+  const [showCPassword, setShowCPasword] = useState(false);
+  const token = sessionStorage.getItem("token");
+  // const role = sessionStorage.getItem('role');
 
   const navigate = useNavigate();
 
@@ -54,14 +52,26 @@ function ChangePassword() {
     setShowCPasword((prevShowConfirmPassword) => !prevShowConfirmPassword);
   };
 
-  const { register, handleSubmit, formState: { errors } } = useForm({
-    resolver: yupResolver(schema)
+
+  const formik = useFormik({
+    initialValues: {
+
+      userName: '',
+      oldPassword: '',
+      newPassword: '',
+      confirmPassword: '',
+    },
+    validationSchema:validationSchema,
+    onSubmit: async (data) => {
+      console.log("Api login Data:", data);
+      handleChangePassword(data)
+    },
   });
 
-  const  handleChangePassword = async (data) => {
+  const handleChangePassword = async (data) => {
     try {
       const response = await axios.post(
-        `${API_URL}changeUserPassword`,data,
+        `${API_URL}changeUserPassword`, data,
         {
           headers: {
             "Content-Type": "application/json",
@@ -90,63 +100,77 @@ function ChangePassword() {
             </div>
             <div className="col-lg-6 col-md-6 col-12">
               <h3 className="registerWord text-center">Change Password</h3>
-              <form onSubmit={handleSubmit(handleChangePassword)}>
-
+              <form onSubmit={formik.handleSubmit}>
                 <div className="form-group mt-3">
                   <label>User Name:</label>
                   <input
-                   {...register('userName')}
                     type="text"
-                    className="form-control"
-                    name="userName"
-                    id="userName"
+                    className={`form-size form-control  ${formik.touched.userName && formik.errors.userName ? 'is-invalid' : ''}`}
+                      {...formik.getFieldProps('userName')}
+                      name="userName"
+                      id="userName"
                   />
-                   <p className='text-danger'>{errors.userName?.message}</p>
+                   {formik.touched.userName && formik.errors.userName && (
+                      <p className="text-danger">{formik.errors.userName}</p>
+                    )}
                 </div>
 
                 <div className="form-group mt-3">
-                    <label className="form-label">Old Password:</label>
-                    <div className="input-group">
-                      <input {...register('oldPassword')}
-                        type={showOldPassword ? "text" : "password"}
-                        className="form-control" id="oldPassword"
-                        style={{ margin: "0px"  }}
-                      />
-                      <span className="input-group-append eye-icon" 
+                  <label className="form-label">Old Password:</label>
+                  <div className="input-group">
+                    <input type={showOldPassword ? "text" : "password"}
+                      className={`form-size form-control  ${formik.touched.oldPassword && formik.errors.oldPassword ? 'is-invalid' : ''}`}
+                      {...formik.getFieldProps('oldPassword')}
+                      name="oldPassword"
+                      id="oldPassword"
+                      style={{ margin: "0px" }}
+                    />
+                    <span className="input-group-append eye-icon"
                       onClick={toggleOldPasswordVisibility}>
-                        {showOldPassword ? <FaEyeSlash /> : <FaEye />}</span>
-                        </div>
-                      <p className='text-danger'>{errors.oldPassword?.message}</p>   
+                      {showOldPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                  </div>
+                  {formik.touched.oldPassword && formik.errors.oldPassword && (
+                      <p className="text-danger">{formik.errors.oldPassword}</p>
+                    )}
                 </div>
 
                 <div className="form-group mt-3">
-                    <label className="form-label">New Password:</label>
-                    <div className="input-group">
-                      <input {...register('newPassword')}
-                        type={showNewPassword ? "text" : "password"}
-                        className="form-control" id="newPassword"
-                        style={{ margin: "0px"  }}
-                      />
-                        <span className="input-group-append eye-icon"  
-                        onClick={toggleNewPasswordVisibility}>
-                        {showNewPassword ? <FaEyeSlash /> : <FaEye />}</span>
-                    </div>
-                      <p className='text-danger'>{errors.newPassword?.message}</p>   
+                  <label className="form-label">New Password:</label>
+                  <div className="input-group">
+                    <input type={showNewPassword ? "text" : "password"}
+                      className={`form-size form-control  ${formik.touched.newPassword && formik.errors.newPassword ? 'is-invalid' : ''}`}
+                      {...formik.getFieldProps('newPassword')}
+                      name="newPassword"
+                      id="newPassword"
+                      style={{ margin: "0px" }}
+                    />
+                    <span className="input-group-append eye-icon"
+                      onClick={toggleNewPasswordVisibility}>
+                      {showNewPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                  </div>
+                  {formik.touched.newPassword && formik.errors.newPassword && (
+                      <p className="text-danger">{formik.errors.newPassword}</p>
+                    )}
                 </div>
 
                 <div className="form-group mt-3">
-                    <label className="form-label">Confirm Password:</label>
-                    <div className="input-group">
-                      <input {...register('confirmPassword')}
-                        type={showCPassword ? "text" : "password"}
-                        className="form-control" id="confirmPassword"
-                        style={{ margin: "0px"  }}
-                      />
-                          <span className="input-group-append eye-icon" 
+                  <label className="form-label">Confirm Password:</label>
+                  <div className="input-group">
+                    <input
+                      type={showCPassword ? "text" : "password"}
+                      className={`form-size form-control  ${formik.touched.confirmPassword && formik.errors.confirmPassword ? 'is-invalid' : ''}`}
+                      {...formik.getFieldProps('confirmPassword')}
+                      name="confirmPassword"
+                      id="confirmPassword"
+                      style={{ margin: "0px" }}
+                    />
+                    <span className="input-group-append eye-icon"
                       onClick={toggleCPasswordVisibility}>
-                        {showCPassword ? <FaEyeSlash /> : <FaEye />}</span>
-                    </div>
-                      <p className='text-danger'>{errors.confirmPassword?.message}</p>   
+                      {showCPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                  </div>
+                  {formik.touched.confirmPassword && formik.errors.confirmPassword && (
+                      <p className="text-danger">{formik.errors.confirmPassword}</p>
+                    )}
                 </div>
 
                 <button
@@ -156,7 +180,6 @@ function ChangePassword() {
                 >
                   Change Password
                 </button>
-
               </form>
             </div>
           </div>
