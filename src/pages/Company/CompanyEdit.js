688c0bb1d4a06b715895a3d43bdd0ca75e7d6a68
sakthiveljayabal0088 <sakthiveljayabal23@gmail.com>
2024-04-13 17:55:57 +0530

    Company Registeration added

---
+++ src/pages/Company/CompanyEdit.js
@@ -0,0 +1,486 @@
+import React, { useEffect, useState } from "react";
+import User from "../../assets/user.png";
+// import axios from "axios";
+import { Link } from "react-router-dom";
+// import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+// import { API_URL } from "../../Config/URL";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import "../../styles/dummy.css";
+import { Password } from "@mui/icons-material";
+import { FaCamera, FaEye, FaEyeSlash } from "react-icons/fa";
+
+const validationSchema = Yup.object().shape({
+  user_name: Yup.string().required("*User Name is required"),
+  company_name: Yup.string().required("*Company Name is required"),
+  phone: Yup.string()
+    .matches(/^\d+$/, "Must be only digits")
+    .min(8)
+    .max(10)
+    .required("*Phone Number is required is required"),
+  email: Yup.string().email("Invalid email").required("*Email is required"),
+  status: Yup.string().required("*Status is required"),
+  role: Yup.string().required("*Role is required"),
+  Password: Yup.string()
+    .required("*Enter the valid Password")
+    .min(4, "*min length of 4 chars")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+      "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character"
+    )
+    .max(10, "*Enter upto 15 chars only"),
+  confirm_Password: Yup.string()
+    .oneOf([Yup.ref("Password"), null], "Passwords must match")
+    .required("*Confirm Password is required"),
+  address: Yup.string().required("*Enter Your Address"),
+  city: Yup.string().required("*Enter Your City"),
+  state: Yup.string().required("*Enter Your State"),
+  zip_code: Yup.string().required("*Enter Country Code Number"),
+  country: Yup.string().required("*Enter Your Country "),
+});
+
+function CompanyEdit() {
+  // const navigate = useNavigate();
+  // const token = sessionStorage.getItem("token");
+  const [userImage, setUserImage] = useState(User);
+  const userId = sessionStorage.getItem("userId");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      company_id: userId,
+      // company_owner: owner,
+      user_name: "",
+      company_name: "",
+      phone: "",
+      email: "",
+      status: "",
+      role: "",
+      Password: "",
+      confirm_Password: "",
+      address: "",
+      city: "",
+      state: "",
+      zip_code: "",
+      country: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (data) => {
+      console.log("Company Datas:", data);
+      //   try {
+      //     const response = await axios.post(`${API_URL}newAccount`, data, {
+      //       headers: {
+      //         "Content-Type": "application/json",
+      //         //Authorization: `Bearer ${token}`,
+      //       },
+      //     });
+      //     if (response.status === 201) {
+      //       toast.success(response.data.message);
+      //       navigate("/accounts");
+      //     } else {
+      //       toast.error(response.data.message);
+      //     }
+      //   } catch (error) {
+      //     toast.error("Failed: " + error.message);
+      //   }
+    },
+  });
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setUserImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <section className="createLead">
+      <form onSubmit={formik.handleSubmit}>
+        <div className="container-fluid">
+          <div className="row mt-3">
+            <div className="col-lg-6 col-md-6 col-12">
+              <h4>
+                <b>Edit Company</b>
+                <br></br>
+                <img
+                  src={userImage}
+                  className="img-fluid mt-3"
+                  style={{
+                    width: "70px",
+                    height: "70px",
+                    cursor: "pointer",
+                    borderRadius: "50%",
+                  }}
+                  alt="user"
+                  onClick={() => document.getElementById("imageInput").click()}
+                />
+                {/* Input for image upload */}
+                <input
+                  type="file"
+                  id="imageInput"
+                  accept="image/*"
+                  style={{ display: "none" }}
+                  onChange={handleImageUpload}
+                />
+                <FaCamera className="cameraIcon" />
+              </h4>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12 d-flex justify-content-lg-end justify-content-md-end">
+              <span>
+                <Link to="/company">
+                  <button className="btn btn-danger">Cancel</button>
+                </Link>
+              </span>
+              &nbsp;
+              <span>
+                <button className="btn btn-primary" type="submit">
+                  Save
+                </button>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid my-5">
+          <h4>
+            <b>Company Information</b>
+          </h4>
+        </div>
+        <div className="container">
+          <div className="row">
+            <input
+              type="hidden"
+              {...formik.getFieldProps("company_id")}
+              value={userId}
+              name="company_id"
+            />
+
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+              <div className="d-flex align-items-center justify-content-end  sm-device">
+                <lable>User Name</lable> &nbsp;&nbsp;
+                <input
+                  {...formik.getFieldProps("user_name")}
+                  type="text"
+                  className="form-size form-control"
+                  id="user_name"
+                  name="user_name"
+                />
+              </div>
+              <div className="row sm-device pb-4">
+                <div className="col-5"></div>
+                <div className="col-6 sm-device">
+                  {formik.touched.user_name && formik.errors.user_name && (
+                    <div className="text-danger ">
+                      {formik.errors.user_name}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+              <div className="d-flex align-items-center justify-content-end  sm-device">
+                <lable>Company Name</lable> &nbsp;&nbsp;
+                <input
+                  {...formik.getFieldProps("company_name")}
+                  type="text"
+                  className="form-size form-control"
+                  id="company_name"
+                  name="company_name"
+                />
+              </div>
+              <div className="row sm-device pb-4">
+                <div className="col-5"></div>
+                <div className="col-6 sm-device">
+                  {formik.touched.company_name &&
+                    formik.errors.company_name && (
+                      <div className="text-danger ">
+                        {formik.errors.company_name}
+                      </div>
+                    )}
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+              <div className="d-flex align-items-center justify-content-end  sm-device">
+                <lable>Phone</lable> &nbsp;&nbsp;
+                <div className="input-group" style={{ width: "60%" }}>
+                  <div>
+                    <select
+                      className="form-size form-select form-control"
+                      {...formik.getFieldProps("country_code")}
+                      style={{
+                        width: "80px",
+                        borderTopRightRadius: "0px",
+                        borderBottomRightRadius: "0px",
+                      }}
+                      name="country_code"
+                    >
+                      <option value="+65">+65</option>
+                      <option value="+91">+91</option>
+                    </select>
+                  </div>
+
+                  <input
+                    {...formik.getFieldProps("phone")}
+                    type="tel"
+                    name="phone"
+                    id="phone"
+                    className="form-control form-size"
+                    aria-label="Text input with checkbox"
+                  />
+                </div>
+              </div>
+
+              <div className="row sm-device pb-4">
+                <div className="col-5"></div>
+                <div className="col-6 sm-device">
+                  {formik.touched.phone && formik.errors.phone && (
+                    <div className="text-danger ">{formik.errors.phone}</div>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+              <div className="d-flex align-items-center justify-content-end  sm-device">
+                <lable>Email</lable> &nbsp;&nbsp;
+                <input
+                  {...formik.getFieldProps("email")}
+                  type="text"
+                  className="form-size form-control"
+                  id="email"
+                  name="email"
+                />
+              </div>
+              <div className="row sm-device pb-4">
+                <div className="col-5"></div>
+                <div className="col-6 sm-device">
+                  {formik.touched.email && formik.errors.email && (
+                    <div className="text-danger ">{formik.errors.email}</div>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+              <div className="d-flex align-items-center justify-content-end  sm-device">
+                <label>Status</label>&nbsp;&nbsp;
+                <select
+                  id="status"
+                  className="form-size form-select"
+                  name="status"
+                >
+                  <option value="--"></option>
+                  <option value="approved">Approved</option>
+                  <option value="pending">Pending</option>
+                  <option value="rejected">Rejected</option>
+                </select>
+              </div>
+              <div className="row sm-device pb-4">
+                <div className="col-5"></div>
+                <div className="col-6 sm-device">
+                  {formik.touched.status && formik.errors.status && (
+                    <div className="text-danger ">{formik.errors.status}</div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+              <div className="d-flex align-items-center justify-content-end  sm-device">
+                <label>Role</label>&nbsp;&nbsp;
+                <select id="roll" className="form-size form-select" name="role">
+                  <option value="--"></option>
+                  <option value="ceo">CEO</option>
+                  <option value="manager">Manager</option>
+                  <option value="others">Others</option>
+                </select>
+              </div>
+              <div className="row sm-device pb-4">
+                <div className="col-5"></div>
+                <div className="col-6 sm-device">
+                  {formik.touched.role && formik.errors.role && (
+                    <div className="text-danger ">{formik.errors.role}</div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+              <div className="d-flex align-items-center justify-content-end  sm-device">
+                <label>Password</label>&nbsp;&nbsp;
+                <div className="input-group " style={{width:"60%"}}>
+                  <input
+                    {...formik.getFieldProps("Password")}
+                    type={showPassword ? "text" : "password"}
+                    className="form-size form-control"
+                    id="Password"
+                    name="Password"
+                  />
+                  <span
+                    className="btn btn-outline-secondary"
+                    onClick={togglePasswordVisibility}
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </span>
+                </div>
+              </div>
+              <div className="row sm-device pb-4">
+                <div className="col-5"></div>
+                <div className="col-6 sm-device">
+                  {formik.touched.Password && formik.errors.Password && (
+                    <div className="text-danger ">{formik.errors.Password}</div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+          <div className="d-flex align-items-center justify-content-end  sm-device">
+            <label>Confirm Password</label>&nbsp;&nbsp;
+            <div className="input-group"  style={{width:"60%"}}>
+              <input
+                {...formik.getFieldProps("confirm_Password")}
+                type={showConfirmPassword ? "text" : "password"}
+                className="form-size form-control"
+                id="confirm_Password"
+                name="confirm_Password"
+              />
+              <button
+                type="button"
+                className="btn btn-outline-secondary"
+                onClick={toggleConfirmPasswordVisibility}
+              >
+                {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+          </div>
+          <div className="row sm-device pb-4">
+            <div className="col-5"></div>
+            <div className="col-6 sm-device">
+              {formik.touched.confirm_Password && formik.errors.confirm_Password && (
+                <div className="text-danger ">{formik.errors.confirm_Password}</div>
+              )}
+            </div>
+          </div>
+        </div>
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+              <div className="d-flex align-items-center justify-content-end  sm-device">
+                <lable>Address</lable> &nbsp;&nbsp;
+                <input
+                  {...formik.getFieldProps("address")}
+                  type="text"
+                  className="form-size form-control"
+                  id="address"
+                  name="address"
+                />
+              </div>
+              <div className="row sm-device pb-4">
+                <div className="col-5"></div>
+                <div className="col-6 sm-device">
+                  {formik.touched.address && formik.errors.address && (
+                    <div className="text-danger ">{formik.errors.address}</div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+              <div className="d-flex align-items-center justify-content-end  sm-device">
+                <lable>City</lable> &nbsp;&nbsp;
+                <input
+                  {...formik.getFieldProps("city")}
+                  type="text"
+                  className="form-size form-control"
+                  id="city"
+                  name="city"
+                />
+              </div>
+              <div className="row sm-device pb-4">
+                <div className="col-5"></div>
+                <div className="col-6 sm-device">
+                  {formik.touched.city && formik.errors.city && (
+                    <div className="text-danger ">{formik.errors.city}</div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+              <div className="d-flex align-items-center justify-content-end  sm-device">
+                <lable>State</lable> &nbsp;&nbsp;
+                <input
+                  {...formik.getFieldProps("state")}
+                  type="text"
+                  className="form-size form-control"
+                  id="state"
+                  name="state"
+                />
+              </div>
+              <div className="row sm-device pb-4">
+                <div className="col-5"></div>
+                <div className="col-6 sm-device">
+                  {formik.touched.state && formik.errors.state && (
+                    <div className="text-danger ">{formik.errors.state}</div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+              <div className="d-flex align-items-center justify-content-end  sm-device">
+                <lable>Zip Code</lable> &nbsp;&nbsp;
+                <input
+                  {...formik.getFieldProps("zip_code")}
+                  type="text"
+                  className="form-size form-control"
+                  id="zip_code"
+                  name="zip_code"
+                />
+              </div>
+              <div className="row sm-device pb-4">
+                <div className="col-5"></div>
+                <div className="col-6 sm-device">
+                  {formik.touched.zip_code && formik.errors.zip_code && (
+                    <div className="text-danger ">{formik.errors.zip_code}</div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12 mb-3">
+              <div className="d-flex align-items-center justify-content-end  sm-device">
+                <lable>Country</lable> &nbsp;&nbsp;
+                <input
+                  {...formik.getFieldProps("country")}
+                  type="text"
+                  className="form-size form-control"
+                  id="country"
+                  name="country"
+                />
+              </div>
+              <div className="row sm-device pb-4">
+                <div className="col-5"></div>
+                <div className="col-6 sm-device">
+                  {formik.touched.country && formik.errors.country && (
+                    <div className="text-danger ">{formik.errors.country}</div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default CompanyEdit;
---
+++ src/pages/Company/CompanyShow.js
@@ -0,0 +1,207 @@
+import React, { useEffect, useState } from "react";
+import "../../styles/Ragul.css";
+import { Link, useNavigate } from "react-router-dom";
+import { BsThreeDots } from "react-icons/bs";
+import USER from "../../assets/user.png";
+import { useParams } from "react-router-dom";
+// import axios from "axios";
+// import { API_URL } from "../../Config/URL";
+
+// import { toast } from "react-toastify";
+import { IoArrowBack } from "react-icons/io5";
+import SendEmail from "../Email/SendEmail";
+import { Tooltip, Zoom } from "@mui/material";
+
+function CompanyShow() {
+  const { id } = useParams();
+  // const token = sessionStorage.getItem("token");
+  const role = sessionStorage.getItem("role");
+  // const [total, setTotal] = useState(0);
+  // const [selectedFile, setSelectedFile] = useState(null);
+  // const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const userData = async () => {
+  //     try {
+  //       const response = await axios(`${API_URL}allAccounts/${id}`, {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //           //Authorization: `Bearer ${token}`,
+  //         },
+  //       });
+  //       setAccountData(response.data);
+  //       // console.log("Account Show :",response.data);
+  //       setTotal(response.data.quotes ? response.data.quotes.length : 0);
+  //     } catch (error) {
+  //       toast.error("Error fetching data:", error);
+  //     }
+  //   };
+
+  //   userData();
+  // }, [id]);
+
+  const handelEdit = () => {
+    navigate(`/company/companyedit`);
+  };
+
+  return (
+    <>
+      {/* header section */}
+      <section className="container-fluid row section1 m-0 p-0">
+        <div className="col-sm-6 py-1">
+          <div className="container">
+            <div className="container-fluid row image-container">
+              <div className="image-container">
+                <Tooltip TransitionComponent={Zoom} title="Back">
+                  <button
+                    className="btn fs-4 border-white"
+                    onClick={() => navigate("/company")}
+                  >
+                    <IoArrowBack className="back_arrow" />
+                  </button>
+                </Tooltip>
+                <img
+                  className="img-fluid"
+                  style={{ width: "5rem" }}
+                  src={USER}
+                  alt="profile"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-6 mt-1" id="buttons-container">
+          <SendEmail />
+
+          <button
+            className={`btn btn-warning ${role === "CMP_USER" && "disabled"}`}
+            disabled={role === "CMP_USER" || role === "CMP_ADMIN"}
+            onClick={handelEdit}
+          >
+            Edit
+          </button>
+
+          <button className="btn bg-light bg-gradient mx-2  text-dark shadow-none">
+            <BsThreeDots />
+          </button>
+        </div>
+      </section>
+
+      <section className="container-fluid row p-3 section2 m-0 p-0 d-flex justify-content-around align-items-center">
+        <div
+          className="container-fluid col-md-9 m-0"
+          id="userDetails-container"
+        >
+          {/* Details */}
+          <div className="container-fluid row" id="Details">
+            <div className="border-bottom py-3">
+              <span className="fs-6 fw-bold my-3"> Details</span>
+            </div>
+
+            <div className="container-fluid col-md-6">
+              <div>
+                <label className="text-dark Label">User Name</label>
+                <span className="text-dark">&nbsp; : &nbsp;Sakthivel</span>
+              </div>
+
+              <div>
+                <label className="text-dark Label">Company Name</label>
+                <span className="text-dark">&nbsp; : &nbsp;Cloud ECS</span>
+              </div>
+
+              <div>
+                <label className="text-dark Label">Email</label>
+                <span className="text-dark">
+                  &nbsp; : &nbsp;sakthivel23@gmail.com
+                </span>
+              </div>
+
+              <div>
+                <label className="text-dark Label">Phone</label>
+                <span className="text-dark">
+                  &nbsp; : &nbsp;&nbsp;6354647382
+                </span>
+              </div>
+            </div>
+
+            <div className="container-fluid col-md-6"></div>
+          </div>
+
+          {/* Hide Details */}
+          <div className="container-fluid row" id="Details">
+            <div className="border-bottom py-3">
+              <span className="fs-6 fw-bold my-3"> Hide Details</span>
+            </div>
+            <div className="py-3">
+              <span className="fs-6 fw-bold"> Company Information</span>
+            </div>
+
+            <div className="container-fluid col-md-12">
+              <div>
+                <label className="text-dark Label">User Name</label>
+                <span className="text-dark">&nbsp; : &nbsp;Sakthivel</span>
+              </div>
+
+              <div>
+                <label className="text-dark Label">Company Name</label>
+                <span className="text-dark">&nbsp; : &nbsp;Cloud ECS</span>
+              </div>
+
+              <div>
+                <label className="text-dark Label">Email</label>
+                <span className="text-dark">
+                  &nbsp; : &nbsp;sakthivel23@gmail.com
+                </span>
+              </div>
+
+              <div>
+                <label className="text-dark Label">Phone</label>
+                <span className="text-dark">
+                  &nbsp; : &nbsp;&nbsp;6354647382
+                </span>
+              </div>
+              <div className="container-fluid col-md-6"></div>
+             
+          </div>
+          <div className="container-fluid col-sm-12">
+              <div className="my-3 container-fluid row d-flex justify-content-between">
+                <span className="my-3 fs-6 fw-bold col-10 my-3">
+                  Address Information
+                </span>
+                <button className="btn bg-info col-2 text-white">
+                  Locate Map
+                </button>
+              </div>
+              <div>
+                <label className="text-dark Label">Address</label>
+                <span className="text-dark">&nbsp; : &nbsp; Chennai</span>
+              </div>
+              <div>
+                <label className="text-dark Label">City</label>
+                <span className="text-dark">&nbsp; : &nbsp;Chennai</span>
+              </div>
+              <div>
+                <label className="text-dark Label">State</label>
+                <span className="text-dark">&nbsp; : &nbsp;Tamil Nadu</span>
+              </div>
+              <div>
+                <label className="text-dark Label">Zip Code</label>
+                <span className="text-dark">&nbsp; : &nbsp;600002</span>
+              </div>
+              <div>
+                <label className="text-dark Label">Country</label>
+                <span className="text-dark">&nbsp; : &nbsp;India</span>
+              </div>
+            </div>
+
+            <div className="container-fluid col-md-6"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default CompanyShow;
