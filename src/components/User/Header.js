60af6dd3b7ba080b86c089e41bb8d98a196336e0
Ragul-dravid <dhurai737@gmail.com>
2024-04-18 21:49:24 +0530

    update logo

--- src/components/User/Header.js
+++ src/components/User/Header.js
@@ -6,8 +6,8 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 import Offcanvas from "react-bootstrap/Offcanvas";
-import Logo from "../../assets/EcsWebsiteLogo.png";
-// import Logo from "../../assets/WebsiteLogo.png";
+// import Logo from "../../assets/EcsWebsiteLogo.png";
+import Logo from "../../assets/WebsiteLogo.png";
 import "../../styles/user.css";
 
 
