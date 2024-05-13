1e3cd74a1c89221bebd64ffb377d1a08d5fb3715
manojecs2020 <manoj@ecscloudinfotech.com>
2024-05-13 19:20:45 +0530

    bug fixed

--- src/pages/Appointments/AppointmentsCreate.js
+++ src/pages/Appointments/AppointmentsCreate.js
@@ -138,7 +138,134 @@ function AppointmentsCreate({ name, schedule, getData }) {
             );
 
             if (response.status === 200) {
-              const mailContent = `
+              let mailContent;
+              if(data.appointmentMode==="ONLINE"){
+                 mailContent =  `
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                  <meta charset="UTF-8" />
+                  <title>Invoice</title>
+                  <style>
+                    body{
+                      background-color: #ddd;
+                    }
+                    .invoice-box {
+                      font-size: 12px;
+                      max-width: 600px;
+                      background-color: #fff;
+                      margin: auto;
+                      padding: 30px;
+                      border-bottom: 3px solid #0059ff;
+                      line-height: 24px;
+                      font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
+                      color: #555;
+                      min-height: 85vh;
+                    }
+  
+                  .invoice-box table {
+                    width: 100%;
+                    line-height: inherit;
+                    text-align: left;
+                  }
+  
+                  .invoice-box table td {
+                    padding: 5px;
+                    vertical-align: top;
+                  }
+  
+                  .invoice-box table td.third {
+                    text-align: right;
+                  }
+  
+                  .invoice-box table tr.heading td {
+                    background: #eee;
+                    border-bottom: 1px solid #ddd;
+                    font-weight: bold;
+                  }
+  
+                  .invoice-box table tr.item td {
+                    border-bottom: 1px solid #eee;
+                  }
+  
+                  .invoice-box table tr.item.last td {
+                    border-bottom: none;
+                  }
+  
+                  .invoice-box table tr.total td:nth-child(2) {
+                    border-top: 2px solid #eee;
+                    font-weight: bold;
+                  }
+                  .invoice{
+                      padding: 1rem;
+                  }
+  
+                  #scan {
+                    float: right;
+                  }
+  
+                  #scan img {
+                    max-width: 100%;
+                    height: auto;
+                  }
+  
+                  @media print {
+                    .invoice-box {
+                      border: 0;
+                    }
+                  }
+  
+                </style>
+                </head>
+                <body >
+                  <div class="invoice-box">
+                    <table>
+                      <tr class="top">
+                        <td colspan="2">
+                          <table>
+                            <tr>
+                              <td class="title">
+                                <img
+                                  src="https://ecscloudinfotech.com/ecs/static/media/ecs_logo.592342beab02474edfc6.png"
+                                  style="width: 75%; max-width: 180px"
+                                  alt="Logo"
+                                />
+                              </td>
+                              <td class="third">
+                                <b>Date:</b> 24-01-2024<br />
+                                The Alexcier, 237 Alexandra Road,<br />
+                                #04-10, Singapore-159929.
+                              </td>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+                    </table>
+  
+  
+                  <div class="invoice" >
+                    <h1 style="color: black;">Hi there, ${data.appointmentOwner}</h1>
+                    <p style="margin: 2rem 0 0;">You've Scheduled An Appointment With ${data.appointmentOwner} for ${data.appointmentName} On 
+                      ${data.appointmentStartDate} at ${data.timeSlotId} <br />(Asia/Kolkata GMT +05:30).
+                    </p>
+  
+                    <h3 style="margin-bottom: 0;">you can join:</h3>
+                    <h4 style="margin:0 ;">${response.data.message}</h4>
+  
+                    <p style="margin: 1.5rem 0px 2rem 0px;"
+                    >You Can Still <span><a href="https://crmlah.com/reschedule/index.html?id=${response.data.appointmentId}">reschedule</a></span> or <a href="https://crmlah.com/cancel/index.html?id=${response.data.appointmentId}">Cancel</a> Your Appointment</p>
+                    <hr />
+                    <p style=" margin: 2rem 0 0;">See You Soon,</p>
+                    <h4 style=" margin: 0; ">${data.appointmentOwner}</h4>
+                    <p style=" margin: 0 ; ">ECS Cloud</p>
+                    <p style=" margin: 0 0 2rem 0;">Powered by ECS</p>
+                    <hr />
+                  </div>
+                  </div>
+                </body>
+                </html>`;
+              }else{
+               mailContent =  `
               <!DOCTYPE html>
               <html lang="en">
               <head>
@@ -253,7 +380,7 @@ function AppointmentsCreate({ name, schedule, getData }) {
                   <p style="margin: 1.5rem 0px 2rem 0px;"
                   >You Can Still <span><a href="https://crmlah.com/reschedule/index.html?id=${response.data.appointmentId}">reschedule</a></span> or <a href="https://crmlah.com/cancel/index.html?id=${response.data.appointmentId}">Cancel</a> Your Appointment</p>
                   <hr />
-                    <p>${response.data.message}</p>
+                  
                   <p style=" margin: 2rem 0 0;">See You Soon,</p>
                   <h4 style=" margin: 0; ">${data.appointmentOwner}</h4>
                   <p style=" margin: 0 ; ">ECS Cloud</p>
@@ -263,6 +390,7 @@ function AppointmentsCreate({ name, schedule, getData }) {
                 </div>
               </body>
               </html>`;
+            }
               try {
                 const response = await axios.post(`${API_URL}sendMail`, {
                   toMail: data.email,
